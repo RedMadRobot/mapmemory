1e1bbcb854b8342108bac17bd1864c8a873e47cb
Osip Fatkullin <o.fatkullin@redmadrobot.com>
2020-11-04 22:03:28 +0300

    ci: Add publication to Bintray and GitHub Packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,27 @@
+import com.redmadrobot.build.extension.credentialsExist
+import com.redmadrobot.build.extension.githubPackages
+import com.redmadrobot.build.extension.isSnapshotVersion
+import com.redmadrobot.build.extension.rmrBintray
+
 plugins {
     val infrastructureVersion = "0.3"
     id("redmadrobot.kotlin-library") version infrastructureVersion apply false
     id("redmadrobot.publish") version infrastructureVersion apply false
+
     id("redmadrobot.detekt") version infrastructureVersion
+    `maven-publish`
 }
 
 subprojects {
     group = "com.redmadrobot.mapmemory"
-    version = "1.0"
+    version = "1.0-SNAPSHOT"
+
+    apply(plugin = "maven-publish")
+
+    publishing {
+        repositories {
+            githubPackages("RedMadRobot/mapmemory")
+            if (!isSnapshotVersion && credentialsExist("bintray")) rmrBintray(project.name)
+        }
+    }
 }
