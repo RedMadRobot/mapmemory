42d3c54c2863a6ec083be3b4e1338e89d6dded55
Osip Fatkullin <o.fatkullin@redmadrobot.com>
2021-03-09 20:44:50 +0300

    mapmemory: Add tests

--- mapmemory/build.gradle.kts
+++ mapmemory/build.gradle.kts
@@ -4,3 +4,9 @@ plugins {
 }
 
 description = "Simple in-memory cache conception built on Map"
+
+dependencies {
+    testImplementation("org.assertj:assertj-core:3.19.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.1")
+}
