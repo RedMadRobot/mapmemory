404d636c553c2730657aafa28b90566ee847dcc4
Osip Fatkullin <o.fatkullin@redmadrobot.com>
2021-03-14 11:44:44 +0300

    test: Add functions with property reference

--- mapmemory-test/src/main/kotlin/Scoped.kt
+++ mapmemory-test/src/main/kotlin/Scoped.kt
@@ -1,7 +1,12 @@
 package com.redmadrobot.mapmemory
 
+import kotlin.reflect.KProperty1
+
 /**
  * Associates the specified [value] with the specified [name] scoped to the given type [T].
+ * ```
+ * memory.putScoped<Foo>("memoizedValue", 42)
+ * ```
  * @see scopedKeyOf
  */
 public inline fun <reified T> MapMemory.putScoped(name: String, value: Any?): Any? {
@@ -14,16 +19,61 @@ public inline fun <reified T> MapMemory.putScoped(name: String, value: Any?): An
 }
 
 /**
- * Returns the value associated with the specified [name] scoped to the given type [T].
+ * Associates the specified [value] with the specified [property] declared in the given type [T].
+ * ```
+ * memory.putScoped(Foo::memoizedValue, 42)
+ * ```
  * @see scopedKeyOf
  */
-public inline fun <reified T, V> MapMemory.getScoped(name: String): V {
+public inline fun <reified T, V> MapMemory.putScoped(property: KProperty1<T, V>, value: V): V {
+    val key = scopedKeyOf(property)
     @Suppress("UNCHECKED_CAST")
-    return get(scopedKeyOf<T>(name)) as V
+    return if (value == null) {
+        remove(key)
+    } else {
+        put(key, value)
+    } as V
 }
 
 /**
- * Creates key with the given [name] scoped to the given type [T].
+ * Returns the value associated with the specified [name] scoped to the given type [T].
+ * ```
+ * memory.getScoped<Foo>("memoizedValue")
+ * ```
+ * @see scopedKeyOf
+ */
+public inline fun <reified T> MapMemory.getScoped(name: String): Any? {
+    return get(scopedKeyOf<T>(name))
+}
+
+/**
+ * Returns the value associated with the specified [property] declared in the given type [T].
+ * ```
+ * memory.getScoped(Foo::memoizedValue)
+ * ```
+ * @see scopedKeyOf
+ */
+public inline fun <reified T, V> MapMemory.getScoped(property: KProperty1<T, V>): V {
+    @Suppress("UNCHECKED_CAST")
+    return get(scopedKeyOf(property)) as V
+}
+
+/**
+ * Creates key for the given [propertyName] scoped to the given type [T].
+ * ```
+ * val key = scopedKeyOf<Foo>("memoizedValue")
+ * assert(key == "Foo#memoizedValue")
+ * ```
  * @see mapMemoryOf
  */
-public inline fun <reified T> scopedKeyOf(name: String): String = "${T::class.java.name}#$name"
+public inline fun <reified T> scopedKeyOf(propertyName: String): String = "${T::class.java.name}#$propertyName"
+
+/**
+ * Creates key for the given [property] declared in the given type [T].
+ * ```
+ * val key = scopedKeyOf(Foo::memoizedValue)
+ * assert(key == "Foo#memoizedValue")
+ * ```
+ * @see mapMemoryOf
+ */
+public inline fun <reified T> scopedKeyOf(property: KProperty1<T, *>): String = "${T::class.java.name}#${property.name}"
