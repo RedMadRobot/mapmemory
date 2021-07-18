c2fc0627165549719e848f6f5812991d99f6b336
Osip Fatkullin <o.fatkullin@redmadrobot.com>
2021-07-18 11:50:17 +0300

    desp: Add kotlin-test dependency

--- mapmemory-test/build.gradle.kts
+++ mapmemory-test/build.gradle.kts
@@ -7,6 +7,8 @@ description = "Utilities helping to test code that uses MapMemory"
 
 dependencies {
     api(project(":mapmemory"))
+
+    testImplementation(kotlin("test-junit5"))
     testImplementation("org.assertj:assertj-core:3.19.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.1")
